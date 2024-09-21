306cebb5b32d989df481f03e521f9fb85e76f7f3
Abhishek Rajput <abhishekrajput9111606916@gmail.com>
2024-09-22 00:20:08 +0530

    improvements

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -18,7 +18,7 @@ const DisplayHome = () => {
             <div className="mb-4">
                 <h1 className='my-5 font-bold'>Today's biggest hits</h1>
                 <div className="flex overflow-auto">
-                    {songsData.map((item, index) => <SongItem key={index} name={item.name} desc={item.desc} image={item.image} />)}
+                    {songsData.map((item, index) => <SongItem key={index} name={item.name} desc={item.desc} image={item.image} id={item.id}/>)}
                 </div>
             </div>
         </>
